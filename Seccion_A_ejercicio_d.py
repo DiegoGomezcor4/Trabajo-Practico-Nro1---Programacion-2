dbcadf06a1e34200d77885cfdd5395494bd16aed
Diego Gomez <diegocor4@gmail.com>
2024-08-22 12:21:54 -0300

    seccion a ejer d completo

--- Seccion_A_ejercicio_d.py
+++ Seccion_A_ejercicio_d.py
@@ -9,3 +9,27 @@ listas(['b', 'a', 'c'], ['e', 'b', 'd', 'c'])
 ['a', 'e', 'd']
 """
 
+def listas_diferencia(lista1, lista2):
+    # Convertir las listas a conjuntos
+    conjunto1 = set(lista1)
+    conjunto2 = set(lista2)
+    
+    # Encontrar elementos comunes
+    elementos_comunes = list(conjunto1.intersection(conjunto2))
+    
+    # Ordenar los elementos comunes en orden inverso
+    elementos_comunes.sort(reverse=True)
+
+    # Encontrar elementos no comunes
+    elementos_no_comunes = list(conjunto1.symmetric_difference(conjunto2))
+
+    # Ordenar los elementos no comunes alfabéticamente
+    elementos_no_comunes.sort()
+    
+    # Imprimir
+    print(elementos_comunes)
+    print(elementos_no_comunes)
+    
+    # Ejemplo
+    
+listas_diferencia(['i', 'a', 'c','e'], ['z','e','b','d','c'])
